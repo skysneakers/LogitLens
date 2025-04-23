6e3f2ed16aff6d3b162bed8f73ebf0cddcea1a48
skysneakers <gwen.lincroft@gmail.com>
2025-04-23 15:05:55 -0400

    Adding setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name = "LogitLens",
+    version = "0.1",
+    description = "API for logit lens plots",
+    author = "Gwenyth Lincroft",
+    author_email = "lincroft.g@northeastern.edu",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='3.11'
+)
